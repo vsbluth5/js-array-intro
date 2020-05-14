b25fd556b278ee371e009e28448284fd3e84bb7f
Glitch (upperlinecode-anagrams-array-methods-javascript-3) <none>
2020-05-14 02:18:52 +0000

    🌚🍛 Checkpoint ./arrays.js:3856783/261

--- arrays.js
+++ arrays.js
@@ -55,8 +55,11 @@ console.log(fortunes.length)
 // 9. Challenge: Out of all the fotunes that are there, it'd mess up the program if some were listed twice, but with a array that big, it could happen.
 // Find a way to check and see whether any of the fortunes are duplicates. If so, find a way to delete those duplicates.
 
-fortunes= [(new Set(fortunes)
-console.log(fortunes.lehgth)
+// fortunes= [new Set(fortunes)]
+
+fortunes.filter((item, index)=>
+                fortunes.indexOf(item) !== index);
+console.log(fortunes.length)
 
 
 
